2d860eaa7a3791a27ea321427a4a5882b888e7c1
Anton Kovalev <a.kovalev@amarkets.com>
2023-02-26 21:07:27 +0100

    add mongodb tutorial

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,3 +1,5 @@
+require('./models/db')
+
 const express = require('express');
 const path = require('path');
 const handlebars = require('handlebars');
@@ -7,17 +9,35 @@ const {
 } = require('@handlebars/allow-prototype-access');
 const bodyparser = require('body-parser');
 
+const studentController = require("./controllers/studentController")
+
 var app = express()
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
-    res.send('
+    res.send(`
     <h2>Welcome to student database!</h2>
-    ')
-})
+    <h3>Click here to get access to the <b> <a href="/student/list">Database</a></b></h3>`);
+});
+
+app.set('views', path.join(__dirname, '/views/'))
+
+app.engine(
+    'hbs', 
+    exphbs.engine({
+        handlebars: allowInsecurePrototypeAccess(handlebars),
+        extname: 'hbs',
+        defaultLayout: 'MainLayout',
+        layoutsDir: __dirname + '/views/layouts/'
+    })
+);
+
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('server started on port 3000');
 });
+
+app.use('/student', studentController);
